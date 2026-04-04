5f0fb793358d46dc12f12d727d223ae9b26625c8
IceFrosTv3 <chaos95kill@gmail.com>
2026-04-04 22:03:21 +0200

    feat: implement ATS-optimized CV page component with print styles

--- src/app/pages/cv/cv.component.ts
+++ src/app/pages/cv/cv.component.ts
@@ -1,15 +1,27 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CvData } from '../../models/cv.interface';
+import { cvEn } from '../../data/cv-en';
+import { cvRu } from '../../data/cv-ru';
+import { cvPl } from '../../data/cv-pl';
+
+const CV_DATA: Record<string, CvData> = {
+  en: cvEn,
+  ru: cvRu,
+  pl: cvPl,
+};
 
 @Component({
   selector: 'app-cv',
   standalone: true,
-  template: '<p>CV works! Lang: {{ lang }}</p>',
+  templateUrl: './cv.component.html',
+  styleUrl: './cv.component.scss',
 })
 export class CvComponent {
-  lang: string;
+  cv: CvData;
 
   constructor(private route: ActivatedRoute) {
-    this.lang = this.route.snapshot.data['lang'];
+    const lang = this.route.snapshot.data['lang'] as string;
+    this.cv = CV_DATA[lang] ?? cvEn;
   }
 }
