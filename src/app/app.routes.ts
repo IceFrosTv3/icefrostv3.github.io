0d724c91bd1c54ee302b69eceade06e76d0dfb21
IceFrosTv3 <chaos95kill@gmail.com>
2026-04-05 14:36:08 +0200

    fixed: title routes and print styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,20 +3,24 @@ import {Routes} from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
+    title: 'Andrei Sergheev | Frontend Developer',
     loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent),
   },
   {
     path: 'cv/en',
+    title: 'CV (EN) — Andrei Sergheev',
     loadComponent: () => import('./pages/cv/cv.component').then(m => m.CvComponent),
     data: {lang: 'en'}
   },
   {
     path: 'cv/ru',
+    title: 'Резюме (RU) — Андрей Сергеев',
     loadComponent: () => import('./pages/cv/cv.component').then(m => m.CvComponent),
     data: {lang: 'ru'}
   },
   {
     path: 'cv/pl',
+    title: 'CV (PL) — Andrei Sergheev',
     loadComponent: () => import('./pages/cv/cv.component').then(m => m.CvComponent),
     data: {lang: 'pl'}
   },
